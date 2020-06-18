8cc6b17fdc3803db7160f4f410d8b792e930e277
DanMillerDev <danmiller126407@gmail.com>
2020-06-18 16:43:24 -0700

    initial commit

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_PrefabToSpawn;
+
+    XRSocketInteractor m_SocketInteractor;
+
+    const float k_Min = 0.05f;
+    const float k_Max = 1.2f;
+
+    void OnEnable()
+    {
+        m_SocketInteractor = GetComponent<XRSocketInteractor>();
+        m_SocketInteractor.onSelectExit.AddListener(ObjectGrabbed);
+    }
+
+    void ObjectGrabbed(XRBaseInteractable interactable)
+    {
+        // object grabbed, spawn another one and set it's size to a random scale between k_Min and k_Max
+        GameObject newSphere = Instantiate(m_PrefabToSpawn, transform.position, Quaternion.identity);
+        newSphere.transform.localScale = GetRandomScale();
+
+    }
+
+    Vector3 GetRandomScale()
+    {
+        float val = UnityEngine.Random.Range(k_Min, k_Max);
+        return new Vector3(val, val, val);
+    }
+}
---
+++ Assets/Scripts/ObjectSticker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ObjectSticker : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    XRGrabInteractable m_GrabInteractable;
+    const string k_StickyWallTag = "StickyWall";
+
+    void OnEnable()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_GrabInteractable = GetComponent<XRGrabInteractable>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag(k_StickyWallTag))
+        {
+            m_Rigidbody.isKinematic = true;
+            Destroy(m_GrabInteractable);
+        }
+    }
+}
